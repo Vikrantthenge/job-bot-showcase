b1cec686418352070b91d2c4f74d4464c5eb9cb6
VIKRANT THENGE <90523497+Vikrantthenge@users.noreply.github.com>
2025-08-19 15:36:04 +0530

    Add files via upload

--- job_search_app.py
+++ job_search_app.py
@@ -11,7 +11,7 @@ import requests
 from PIL import Image
 
 # --- Page Setup ---
-st.set_page_config(page_title="Job Search Bot", layout="wide")
+#st.set_page_config(page_title="Job Search Bot", layout="wide")
 
 # --- Logo + Theme Phrase ---
 from PIL import Image
