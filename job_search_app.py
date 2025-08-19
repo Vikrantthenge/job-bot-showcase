498d848f34eb2edfc2e63d906d192f2f044d23d1
VIKRANT THENGE <90523497+Vikrantthenge@users.noreply.github.com>
2025-08-19 13:56:06 +0530

    Add files via upload

--- job_search_app.py
+++ job_search_app.py
@@ -27,7 +27,7 @@ except FileNotFoundError:
 st.markdown("""
     <hr style="margin-top: 2rem; margin-bottom: 0.5rem;">
     <div style='text-align: center; font-size: 36px; color: #8B0000; font-weight: 500;'>
-         💼📄 <b>Data Job Finder + Resume Uploader</b><br>
+         💼📄<b>Data Job Finder + Resume Uploader</b><br>
         <span style='font-size: 14px; font-weight: 400;'>&copy; 2025 VIKRANT THENGE. All rights reserved.</span>
     </div>
 """, unsafe_allow_html=True)
