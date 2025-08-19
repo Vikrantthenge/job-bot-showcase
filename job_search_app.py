6a3afd0832a0a4a8b4e882c7f758db471b7742fe
VIKRANT THENGE <90523497+Vikrantthenge@users.noreply.github.com>
2025-08-19 14:38:05 +0530

    Add files via upload

--- job_search_app.py
+++ job_search_app.py
@@ -80,10 +80,10 @@ if st.button("🚀 Auto-Apply to All"):
     else:
         st.error("Please upload your resume first.")
 
-# Add styled copyright footer
-# st.markdown("""
-   #  <hr style="margin-top: 2rem; margin-bottom: 0.5rem;">
-    # <div style='text-align: center; font-size: 12px; color: #8B0000;'>
-      #   © 2025 VIKRANT Thenge. All rights reserved.
-    # </div>
-#  """, unsafe_allow_html=True)
+Add styled copyright footer
+st.markdown("""
+   <hr style="margin-top: 2rem; margin-bottom: 0.5rem;">
+    <div style='text-align: center; font-size: 12px; color: #8B0000;'>
+        © 2025 VIKRANT THENGE. All rights reserved.
+    </div>
+""", unsafe_allow_html=True)
